be642ca425e932bac45f23b887826e0273a4918e
dm00z <tt01999@gmail.com>
2015-03-09 17:38:46 +0700

    Fixed batch files

--- Sources/KinectCam/install.bat
+++ Sources/KinectCam/install.bat
@@ -1,5 +1,8 @@
-@c:
-@cd c:\KinectCamV2BI\
+@set DRV=%~d0
+@%DRV%
+
+@set OLDDIR=%~dp0
+@cd %OLDDIR%
 
 @c:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe install BaseClassesNET.dll
 @c:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe install KinectCamBodyIndex.dll
--- Sources/KinectCam/uninstall.bat
+++ Sources/KinectCam/uninstall.bat
@@ -1,8 +1,11 @@
-@c:
-@cd c:\KinectCamV2BI\
+@set DRV=%~d0
+@%DRV%
+
+@set OLDDIR=%~dp0
+@cd %OLDDIR%
 
 @c:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister /nologo BaseClassesNET.dll
-@c:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister /nologo KinectCam.dll
+@c:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister /nologo KinectCamBodyIndex.dll
 
 @c:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe uninstall BaseClassesNET.dll
-@c:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe uninstall KinectCam.dll
+@c:\Windows\Microsoft.NET\Framework\v4.0.30319\ngen.exe uninstall KinectCamBodyIndex.dll
